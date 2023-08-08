c6419773feb5bc7f705c51a981af4afff7da3a47
Omar Abouelkheir <omar@abouelkheirnz.com>
2023-08-08 15:11:54 +1200

    Created letterTonumber function

--- car-value.ts
+++ car-value.ts
@@ -4,5 +4,12 @@ type CarModel = {
 }
 
 const carValue =  (car: CarModel) => {
-    
+
+}
+
+//Take a single letter and return its numberical place in alphabet
+type ConvertAlphabet = (letter: String) => number;
+const letterToNumber: ConvertAlphabet = (letter) => {
+    letter = letter.toUpperCase();
+    return letter.charCodeAt(0)-64;
 }
