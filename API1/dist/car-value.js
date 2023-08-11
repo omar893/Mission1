a7568e72e37fb0d5d990f891d713cb7263a3a8d6
Omar Abouelkheir <omar@abouelkheirnz.com>
2023-08-11 15:06:58 +1200

    Creater API1 folder and made a TypeScript server

---
+++ API1/dist/car-value.js
@@ -0,0 +1,14 @@
+"use strict";
+const carValue = (car) => {
+    return car.model.split('').reduce((acc, curr) => acc + letterToNumber(curr), 0)
+        * 100 + car.year;
+};
+//Take a single letter and return its numberical place in alphabet
+const letterToNumber = (letter) => {
+    letter = letter.toUpperCase();
+    const letterValue = letter.charCodeAt(0) - 64;
+    if (letterValue > 0 && letterValue < 27) {
+        return letterValue;
+    }
+    return 0;
+};
